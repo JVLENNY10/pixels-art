09827d1ad6cd2c5503f2eb50bc81116e01729af7
João Victor <joao.lenny100@gmail.com>
2021-09-12 13:41:47 -0300

    Requisito 6

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
 window.onload = function () {
-  document.querySelector('#black').classList.add('select');
-}
+  document.querySelector('#black').classList.add('selected');
+};
 
 function paletteColor(numberOfColors) {
+  let color;
   const colorItem = document.querySelector('#color-palette');
 
-  let color;
   for (let index = 1; index < numberOfColors; index += 1) {
     color = document.createElement('div');
-
     if (index === 1) {
       color.id = 'black';
     } else if (index === 2) {
       color.id = 'cyan';
     } else if (index === 3) {
       color.id = 'green';
-    } else if (index === 4) {
+    } else {
       color.id = 'red';
     }
-
     colorItem.appendChild(color).classList.add('color');
   }
 
@@ -26,9 +24,9 @@ function paletteColor(numberOfColors) {
 }
 
 function pixelBoard(numberOfPixels) {
+  let onePixel;
   const whitePixel = document.querySelector('#pixel-board');
 
-  let onePixel;
   for (let index = 0; index < numberOfPixels; index += 1) {
     onePixel = document.createElement('div');
     whitePixel.appendChild(onePixel).classList.add('pixel');
@@ -41,14 +39,14 @@ function pixelBoard(numberOfPixels) {
 //   const select = document.querySelector('.color');
 
 //   if (selection.target.id === 'black') {
-//     selection.target.classList.toggle('select');
+//     selection.target.classList.toggle('selected');
 //   } else if (selection.target.id === 'cyan') {
-//     selection.target.classList.toggle('select');
+//     selection.target.classList.toggle('selected');
 //   } else if (selection.target.id === 'green') {
-//     selection.target.classList.toggle('select');
+//     selection.target.classList.toggle('selected');
 //   } else if (selection.target.id === 'red') {
-//     selection.target.classList.toggle('select');
-//   } 
+//     selection.target.classList.toggle('selected');
+//   }
 
 //   return selection;
 // }
