0d5ec0cdb34a415a09a966c62b90bda93cc0adc3
João Victor <joao.lenny100@gmail.com>
2021-09-12 15:58:48 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.onload = function () {
+window.onload = function init() {
   document.querySelector('#black').classList.add('selected');
 };
 
@@ -6,7 +6,7 @@ function paletteColor(numberOfColors) {
   const colorItem = document.querySelector('#color-palette');
 
   for (let index = 1; index < numberOfColors; index += 1) {
-    let color = document.createElement('div');
+    const color = document.createElement('div');
     if (index === 1) {
       color.id = 'black';
     } else if (index === 2) {
@@ -26,30 +26,45 @@ function pixelBoard(numberOfPixels) {
   const whitePixel = document.querySelector('#pixel-board');
 
   for (let index = 0; index < numberOfPixels; index += 1) {
-    let onePixel = document.createElement('div');
+    const onePixel = document.createElement('div');
     whitePixel.appendChild(onePixel).classList.add('pixel');
   }
 
   return whitePixel;
 }
 
-// function selectColor(selection) {
-//   const select = document.querySelector('.color');
+function selectColor(selection) {
+  const black = document.querySelector('#black');
+  const cyan = document.querySelector('#cyan');
+  const green = document.querySelector('#green');
+  const red = document.querySelector('#red');
 
-//   if (selection.target.id === 'black') {
-//     selection.target.classList.toggle('selected');
-//   } else if (selection.target.id === 'cyan') {
-//     selection.target.classList.toggle('selected');
-//   } else if (selection.target.id === 'green') {
-//     selection.target.classList.toggle('selected');
-//   } else if (selection.target.id === 'red') {
-//     selection.target.classList.toggle('selected');
-//   }
+  if (selection.target.id === 'black') {
+    cyan.classList.remove('selected');
+    green.classList.remove('selected');
+    red.classList.remove('selected');
+    selection.target.classList.toggle('selected');
+  } else if (selection.target.id === 'cyan') {
+    black.classList.remove('selected');
+    green.classList.remove('selected');
+    red.classList.remove('selected');
+    selection.target.classList.toggle('selected');
+  } else if (selection.target.id === 'green') {
+    black.classList.remove('selected');
+    cyan.classList.remove('selected');
+    red.classList.remove('selected');
+    selection.target.classList.toggle('selected');
+  } else if (selection.target.id === 'red') {
+    black.classList.remove('selected');
+    cyan.classList.remove('selected');
+    green.classList.remove('selected');
+    selection.target.classList.toggle('selected');
+  }
 
-//   return selection;
-// }
+  return selection;
+}
 
-// document.addEventListener('click', selectColor);
+document.addEventListener('click', selectColor);
 
 pixelBoard(25);
 paletteColor(5);
